ee2de2581af4b09d9bcdf4d7d944d47643b68866
Chriz <25889057+Chriz-G@users.noreply.github.com>
2025-09-30 00:26:45 +0100

    Add UserManagement.Domain project and update references.

---
+++ UserManagement.Domain/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.Domain.Models;
+
+public class UserModel
+{
+    public long Id { get; set; }
+    public required string Forename { get; set; }
+    public required string Surname { get; set; }
+    public required string Email { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
